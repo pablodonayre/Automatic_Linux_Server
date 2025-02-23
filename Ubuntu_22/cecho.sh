abadbca249591f9db3df25a7ee4b9fe2743b895c
K1mberly <kim.ruiz.g@gmail.com>
2025-02-23 12:52:51 -0500

    styling

---
+++ Ubuntu_22/cecho.sh
@@ -0,0 +1,12 @@
+cecho(){
+    RED="\033[0;31m"
+    GREEN="\033[0;32m"  # <-- [0 means not bold
+    YELLOW="\033[1;33m" # <-- [1 means bold
+    CYAN="\033[1;36m"
+    # ... Add more colors if you like
+
+    NC="\033[0m" # No Color
+
+    # printf "${(P)1}${2} ${NC}\n" # <-- zsh
+    printf "${!1}${2} ${NC}\n" # <-- bash
+}
